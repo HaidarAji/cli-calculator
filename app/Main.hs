c51f759411635fd28b003f6899e81858eb76677c
Raden Haidar Aji Prabowo <radenhaidar.prabowo@gmail.com>
2025-02-27 13:20:22 +0700

    Add basic functionality

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main (main) where
 
-import Lib
+import Lib (evaluate)
 
 main :: IO ()
-main = someFunc
+main = do
+    mapM_ putStrLn greeting
+    loop
+
+greeting :: [String]
+greeting = [
+        "Welcome to Haskell CLI Calculator"
+        , "Enter an expression (or type 'exit' to quit)"
+    ]
+
+loop :: IO ()
+loop = do 
+    putStr "> "
+    -- hFlush stdout
+    input <- getLine
+    if input == "exit"
+        then putStrLn "Goodbye!"
+        else do
+            case evaluate input of
+                Right x -> putStrLn $ "Result: " ++ show x
+                Left y -> putStrLn y
+            loop
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,25 @@
 module Lib
-    ( someFunc
+    ( evaluate
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Text.Read (readMaybe)
+
+safeDiv :: (Eq a, Fractional a) => a -> a -> Maybe a 
+safeDiv _ 0 = Nothing
+safeDiv x y = Just ( x / y )
+
+evaluate :: String -> Either String Double
+evaluate input =
+    case words input of
+        [a, op, b] -> case (readMaybe a, readMaybe b) of
+            (Just x, Just y) -> case op of
+                "+" -> Right $ (+) x y
+                "-" -> Right $ (-) x y
+                "*" -> Right $ (*) x y
+                "/" -> case safeDiv x y of
+                    Just z -> Right z
+                    Nothing -> Left "Error: Division by zero"
+                _ -> Left "Error: Unsupported operator"
+            _ -> Left "Error: Invalid numbers"
+        _ -> Left "Error: Invalid input format"
+            
